7f74f675466f7764612e3ea227708f64f4466b52
Hussein <hussein.albreihi@hotmail.com>
2014-09-22 14:28:29 +0200

    added code

--- js/typingSpeed.js
+++ js/typingSpeed.js
@@ -1,5 +1,6 @@
 var poangPlayer1 = 0;
 var poangPlayer2 = 0;
+var slutPoang;
 var name = [];
 var player = 0;
 var words = [
@@ -7,36 +8,27 @@ var words = [
 	'apple',
 	'orange'
 ];
+var rndWordPlayer1, rndWordsPlayer1, rndWordPlayer2, rndWordsPlayer2, userTypingPlayer1, userTypingPlayer2, reset;
 
-var rndWordPlayer1 = Math.floor(Math.random() * words.length);
-var rndWordsPlayer1 = words[rndWordPlayer1];
-var rndWordPlayer2 = Math.floor(Math.random() * words.length);
-var rndWordsPlayer2 = words[rndWordPlayer2];
 var startGame = prompt('To star game please enter GO');
 startGame = startGame.toUpperCase();
-
-if (player === 0) {
-	var userTyping = prompt('Type the word before the time runs out! ' + rndWordsPlayer1 + ' Poäng: ' + poangPlayer1);
-} else {
-	userTyping = prompt('Type the word before the time runs out! ' + rndWordsPlayer2 + ' Poäng: ' + poangPlayer2);
-}
 
 var typingPlayer1 = function () {
 	rndWordPlayer1 = Math.floor(Math.random() * words.length);
 	rndWordsPlayer1 = words[rndWordPlayer1];
-	var userTypingPlayer1 = prompt('Type the word before the time runs out! ' + rndWordsPlayer1 + ' Poäng: ' + poangPlayer1);
+	userTypingPlayer1 = prompt('Type the word before the time runs out! ' + rndWordsPlayer1 + ' Poäng: ' + poangPlayer1);
 };
 
 var typingPlayer2 = function () {
 	rndWordPlayer2 = Math.floor(Math.random() * words.length);
 	rndWordsPlayer2 = words[rndWordPlayer2];
-	var userTypingPlayer2 = prompt('Type the word before the time runs out! ' + rndWordsPlayer2 + ' Poäng: ' + poangPlayer2);
+	userTypingPlayer2 = prompt('Type the word before the time runs out! ' + rndWordsPlayer2 + ' Poäng: ' + poangPlayer2);
 };
 
-if (startGame === 'GO') {
+var gameReset = function () {
 	if (player === 0) {
 		for (var i = 0; i <= 3; i++) {
-			if (userTyping === rndWordsPlayer1) {
+			if (userTypingPlayer1 === rndWordsPlayer1) {
 				console.log('grattis');
 				poangPlayer1 = poangPlayer1 + 1;
 				typingPlayer1();
@@ -49,8 +41,8 @@ if (startGame === 'GO') {
 		player = player + 1;
 	}
 	if (player === 1) {
-		for (var i = 0; i <= 3; i++) {
-			if (userTyping === rndWordsPlayer2) {
+		for (var y = 0; y <= 3; y++) {
+			if (userTypingPlayer2 === rndWordsPlayer2) {
 				console.log('grattis');
 				poangPlayer2 = poangPlayer2 + 1;
 				typingPlayer2();
@@ -60,8 +52,29 @@ if (startGame === 'GO') {
 				typingPlayer2();
 			}
 		}
+		if (poangPlayer1 >= poangPlayer2) {
+			alert('Grattis! Player1 vann!');
+		} else {
+			alert('Grattis! Player2 vann!');
+		}
+		player = player + 1;
 	}
+	if (player === 2) {
+		reset = prompt('would you like to play again? YES or NO');
+	}
+	if (reset === 'YES') {
+		poangPlayer1 = 0;
+		poangPlayer2 = 0;
+		player = 0;
+		gameReset();
+	} else {
+		alert('Plz donate money 4 weed');
+	}
+};
 
+
+if (startGame === 'GO') {
+	gameReset();
 } else {
-	startGame = prompt('To star game please enter GO');
+	prompt('please write GO to start game');
 }
